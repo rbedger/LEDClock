1b7db63340b0e59ed4f43f5670d6bf5fdac80a83
Robert Edger <rbedger@gmail.com>
2021-06-05 17:33:33 -0500

    fill solid before setting digits

--- clock.cpp
+++ clock.cpp
@@ -31,8 +31,19 @@ void Clock::handle(CRGB* leds) {
     Serial.print("Minute: ");
     Serial.println(minuteNow);
 #endif
+    //if (_prev_hour != hourNow || _prev_minute != minuteNow) {
+    //    _font.drawDigit(leds, hourNow / 10, 2, 0);
+    //    _font.drawDigit(leds, hourNow - (hourNow / 10) * 10, 6, 0);
+    //    _font.drawSeparator(leds, 10, 0);
+    //    _font.drawDigit(leds, minuteNow / 10, 12, 0);
+    //    _font.drawDigit(leds, minuteNow - (minuteNow / 10) * 10, 16, 0);
+    //}
+
+    //_prev_hour = hourNow;
+    //_prev_minute = minuteNow;
 
     if (_prev_hour != hourNow || _prev_minute != minuteNow) {
+        fill_solid(_leds_previous, _num_leds, CRGB::Black);
         uint16_t prev_time_offset = 60 * _prev_hour + _prev_minute;
         _font.drawDigit(_leds_previous, _prev_hour / 10, 2, prev_time_offset);
         _font.drawDigit(_leds_previous, _prev_hour - (_prev_hour / 10) * 10, 6, prev_time_offset);
@@ -40,12 +51,14 @@ void Clock::handle(CRGB* leds) {
         _font.drawDigit(_leds_previous, _prev_minute / 10, 12, prev_time_offset);
         _font.drawDigit(_leds_previous, _prev_minute - (_prev_minute / 10) * 10, 16, prev_time_offset);
 
+        fill_solid(_leds_new, _num_leds, CRGB::Black);
         uint16_t time_offset = 60 * hourNow + minuteNow;
         _font.drawDigit(_leds_new, hourNow / 10, 2, time_offset);
         _font.drawDigit(_leds_new, hourNow - (hourNow / 10) * 10, 6, time_offset);
         _font.drawSeparator(_leds_new, 10, time_offset);
         _font.drawDigit(_leds_new, minuteNow / 10, 12, time_offset);
         _font.drawDigit(_leds_new, minuteNow - (minuteNow / 10) * 10, 16, time_offset);
+
         _fade = millis() + 5000;
     }
     _prev_hour = hourNow;
@@ -56,6 +69,15 @@ void Clock::handle(CRGB* leds) {
         if (_fade == 0 || millis() > _fade) {
             amount = 255;
         }
+
+        // fade black faster
+        if (((int)leds[i]) == CRGB::Black) {
+            if (amount >= 128) {
+                amount = 255;
+            }
+            amount *= 2;
+        }
+
         leds[i] = blend(_leds_previous[i], _leds_new[i], amount);
     }
 }
